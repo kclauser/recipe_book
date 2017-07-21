5640f5838ef0cb7c32aa79022eed85f7b9123a1c
Kurtis Clauser <kclauser@gmail.com>
2017-07-21 16:28:38 -0400

    Basic CRUD complete on recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,11 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update, :destory]
-  def index
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @recipe = Recipe.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -18,8 +22,22 @@ class RecipesController < ApplicationController
     end
   end
 
-  def show
+  def edit
   end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: 'Successfully deleted recipe'
+  end
+
 
   private
 
